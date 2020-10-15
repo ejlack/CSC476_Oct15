20f3e6c726178a3ddd14f260426d6f3375a99b01
James Lack <ejlack@outlook.com>
2020-10-15 14:42:06 -0400

    Added BallForce script

---
+++ Assets/BallForce.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallForce : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrustX = 3.0f;
+    public float thrustY = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Update time :" + Time.deltaTime);
+    }
+
+    void FixedUpdate(){
+        rb.AddForce(Input.GetAxis("Horizontal") * thrustX, 0, 0);
+        rb.AddForce(0, Input.GetAxis("Vertical") * thrustY, 0);
+    }
+}
